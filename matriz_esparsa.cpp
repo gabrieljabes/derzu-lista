4615d0cfbe42eae208af83a6ccc175dfa9400bd4
Gabriel Jabes <gabriel.jabesldl@gmail.com>
2025-11-19 14:30:38 -0300

    lista finalizada!

---
+++ matriz_esparsa.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* Implemente uma função que receba como parâmetro de entrada
+uma matriz de números inteiros de tamanho qualquer e o seu tamanho.
+A função a ser implementada deve retornar verdadeiro
+caso a matriz seja esparsa ou falso caso contrário,
+considerando que uma matriz é esparsa se ela possuir
+mais do que 70% dos seus elementos iguais a 0.
+A função main deve ler as dimensões da matriz, cada
+um dos seus elementos e imprimir uma mensagem
+informando se a matriz é esparsa ou não, de acordo
+com a resposta da função. 
+
+A matriz tem largura/altura
+máximas igual a 10.*/
+
+float razaoMatriz(vector <vector<int>> *matriz);
+void preencherMatriz(vector <vector<int>> *matriz);
+
+int main(){
+    int m, n;
+
+    cin >> m;
+    cin >> n;
+
+    vector <vector<int>> matriz (m, vector<int>(n)); 
+
+    preencherMatriz(&matriz);
+
+    float razao = razaoMatriz(&matriz);
+    
+    cout << razao << endl;
+
+    if(razao > 0.7)
+        cout << "A matriz é esparsa" << endl;
+    else
+        cout << "A matriz não é esparsa" << endl;
+
+    return 0;
+}
+
+float razaoMatriz(vector <vector<int>> *matriz){
+    int count_0{};
+    size_t m{matriz->size()};
+    size_t n{matriz->at(0).size()};
+
+    for(size_t i = 0; i < m; i++)
+        for(size_t j = 0; j < n; j++)
+            if(matriz->at(i).at(j) == 0)
+                count_0++;
+
+    return (float)count_0/(m * n);
+}
+
+void preencherMatriz(vector <vector<int>> *matriz){
+
+    size_t m{matriz->size()};
+    size_t n{matriz->at(0).size()};
+
+    for(size_t i = 0; i < m; i++)
+        for(size_t j = 0; j < n; j++)
+            cin >> matriz->at(i).at(j);
+}
